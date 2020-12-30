16f58650c0d7cdc6be99e7540239d4050c768537
Hovac <32835549+Hovsky@users.noreply.github.com>
2020-12-30 23:10:40 +0100

    sarcastic messages to signal user the file is being processed

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ client = discord.Client()
 
 civCode = ["Britons", "Franks", "Goths", "Teutons", "Japanese", "Chinese", "Byzantines", "Persian", "Saracens", "Turks", "Vikings", "Mongols", "Celts", "Spanish", "Aztecs", "Mayans", "Huns", "Koreans", "Italians", "Indians", "Incas", "Magyars", "Slav", "Portuguese", "Ethiopians", "Malians", "Berbers", "Khmer", "Malay", "Burmese", "Vietnamese", "Bulgarians", "Tatars", "Cumans", "Lithuanians"]
 
+randomLines = [
+    "Who said mangoes grow on trees? They grow from siege workshops, let me check if you grew some", "Match didn't start in post-imp, so give me some time to watch you get there and I’ll tell you how bad you did soon.","Wait for me, I’m an old bot, it takes me a bit of time to watch your boring game.", "It takes a few seconds for me to watch your game, I have to stop and re-watch every miss-click you make.","Dude, give me a minute to process your game, it made me fall asleep a few times.","error 404: epic mango shot not found. Deleting your account", "are you sure you want others to watch this game?!", "ERROR 42069: Bad_Plays integer overflow", "Current game time: 1h6m24s - Current vill number: 69 - Idle time: indefinite", "I am actually kidnapped, to watch replays and report score, please send help befo-"
+]
+
 @client.event
 async def on_message(msg):
     if msg.attachments:
         if msg.attachments[0].url.endswith("aoe2record"):
+            random.seed()
+            await msg.channel.send(0, random.randint(0, len(randomLines)))
+
             r = requests.get(msg.attachments[0].url)
             open("currentDLGame.aoe2record", "wb").write(r.content)
 
@@ -52,7 +59,6 @@ async def on_message(msg):
             
             #await msg.channel.send("> Map: **" + str(pMap["name"]) + "**\n>" + " "*25 + "Won" + " "*25 + "**-**" + " "*25 + "Lost" + " "*25 + "\n" + endString)
             embed = discord.Embed(title = "Map: ||" + str(pMap["name"]) + "||")
-            random.seed()
             if random.randint(0,1) == 1:
                 embed.add_field(name = "Winner:", value = "||**Team 1**||", inline= False)
                 embed.add_field(name = "Team 1", value = wTeam, inline = True)
